d669d95232653299f1ccdc5eb9eeb7ba0d5ad867
Emmanuel ZIMMERT <ez@ladigitale.dev>
2022-02-25 21:50:34 +0100

    Update dist

--- dist/inc/televerser_fichier.php
+++ dist/inc/televerser_fichier.php
@@ -13,12 +13,19 @@ if (!empty($_FILES['blob']) && !empty($_POST['parcours'])) {
 	if (!file_exists('../fichiers/' . $parcours)) {
 		mkdir('../fichiers/' . $parcours, 0775, true);
 	}
-	$nom = hash('md5', $_FILES['blob']['tmp_name']) . time() . '.' . $extension;
-	$chemin = '../fichiers/' . $parcours . '/' . $nom;
+	$nom = hash('md5', $_FILES['blob']['tmp_name']) . time();
+	$chemin = '../fichiers/' . $parcours . '/' . $nom . '.' . $extension;
 	if (move_uploaded_file($_FILES['blob']['tmp_name'], $chemin)) {
+		if (in_array($extension, array('jpg', 'jpeg', 'png', 'gif'))) {
+			$cheminvignette = '../fichiers/' . $parcours . '/vignette_' . $nom . '.' . $extension;
+			creer_vignette($chemin, $cheminvignette, 250);
+		}
 		if ($ancienfichier !== '' && $fichier !== $ancienfichier) {
 			if (file_exists('../fichiers/' . $parcours . '/' . $ancienfichier)) {
 				unlink('../fichiers/' . $parcours . '/' . $ancienfichier);
+			}
+			if (file_exists('../fichiers/' . $parcours . '/vignette_' . $ancienfichier)) {
+				unlink('../fichiers/' . $parcours . '/vignette_' . $ancienfichier);
 			}
 		}
 		echo $nom;
@@ -28,6 +35,40 @@ if (!empty($_FILES['blob']) && !empty($_POST['parcours'])) {
 	exit();
 } else {
 	header('Location: /');
+}
+
+function creer_vignette($src, $dest, $h) {
+    $fparts = pathinfo($src);
+    $ext = strtolower($fparts['extension']);
+    if ($ext == 'gif') {
+        $resource = imagecreatefromgif($src);
+	} else if ($ext === 'png') {
+        $resource = imagecreatefrompng($src);
+	} else if ($ext === 'jpg' || $ext === 'jpeg') {
+        $resource = imagecreatefromjpeg($src);
+	}
+    $width  = imagesx($resource);
+    $height = imagesy($resource);
+    $w  = floor($width * ($h / $height));
+    $img = imagecreatetruecolor($w, $h);
+	if ($ext === 'png') {
+		imagealphablending($img, false);
+		imagesavealpha($img, true);
+	}
+    imagecopyresampled($img, $resource, 0, 0, 0, 0, $w, $h, $width, $height);
+    $fparts = pathinfo($dest);
+    $ext = strtolower($fparts['extension']);
+    if (!in_array($ext, array('jpg', 'jpeg', 'png', 'gif'))) {
+		$ext = 'jpg';
+	}
+    $dest = $fparts['dirname'] . '/' . $fparts['filename'] . '.' . $ext;
+    if ($ext == 'gif') {
+        imagegif($img, $dest);
+	} else if ($ext === 'png') {
+        imagepng($img, $dest, 1);
+	} else if ($ext === 'jpg' || $ext === 'jpeg') {
+        imagejpeg($img, $dest, 85);
+	}
 }
 
 ?>
